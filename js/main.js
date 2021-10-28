0c1904364c671c0f6d43992ad69ac179889a28df
Manuel <m.rodriguezb.2018@alumnos.urjc.es>
2021-10-28 18:45:33 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -92,7 +92,7 @@
 		        $(form).ajaxSubmit({
 		            type: "POST",
 		            data: $(form).serialize(),
-		            url : "mail.php",
+		            url : "bananaslipdev@gmail.com",
 		            success: function() {
 		                $(".contact-form").fadeTo( "slow", 1, function() {
 		                    $(".contact-form .msg-success").slideDown();
